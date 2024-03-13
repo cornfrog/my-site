f28d2d9de13073f100f9455297446eba7ecf4a3f
cornfrog <thecornfrog@gmail.com>
2024-03-13 18:19:08 -0400

    added starting layout for homepage and components to be used

---
+++ app/components/Achievements.tsx
@@ -0,0 +1,5 @@
+export default function Achievements() {
+    return (
+        <h1>Hi from Achievements</h1>
+    );
+}
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+        <div className="navbar">
+            <a href="#" className="navbar-link">Top</a>
+            <a href="#" className="navbar-link">Achievements</a>
+            <a href="#" className="navbar-link">Projects</a>
+            <a href="#" className="navbar-link">FAQ</a>
+        </div>
+    );
+}
