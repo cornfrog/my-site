73720c76accc4c3ee976b6be3869ce6d1ec162ff
cornfrog <thecornfrog@gmail.com>
2024-03-14 22:42:45 -0400

    fixed site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Yoyoyoyoyo!",
+  title: "Welcome!",
   description: "Corey Pierce software dev profolio site",
 };
 
