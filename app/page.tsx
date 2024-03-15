e251e8a697fc972547824eb223206d809d54dfd3
cornfrog <thecornfrog@gmail.com>
2024-03-15 09:01:54 -0400

    removed tailwinds. not using it anymore

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,16 @@ import FAQ from "./components/FAQ";
 export default function Home() {
   return (
     <div id="top-of-page">
-      <h1>Welcome!</h1>
-      <div>
-        <div>
+      <h1 className="text-center text-4xl">Welcome!</h1>
+        <div className="">
           <img src="linkedin-pic.jpg" alt="linkedin-pic"/>
-          <p>
+          <p className="">
             My name is Corey Pierce and I am an aspiring software developer. To put it short
             I am an overall tech nerd! My main interests are Web Development, Data
             Analysis, Web3, and Cyber Security.This site is to showcase my past and current 
             projects. I hope you enjoy :)
           </p>
         </div>
-      </div>
       <Connect />
       <NavBar />
       <Projects />
