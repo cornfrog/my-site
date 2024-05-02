f66c00fb93d2b70679dd5c045f8e750452e2fe07
Cornfrog <iamcornfrog@gmail.com>
2024-05-02 17:43:49 -0400

    added pdf viewer to resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,11 +1,14 @@
 export default function Resume() {
-    return(
+    return (
         <div className="page resume">
             <h1 className="resume__header">Resume</h1>
             <a href="/Corey_Pierce-Resume.pdf" download="Corey_Pierce-Resume.pdf" className="resume__download-resume">
-                <img src="download-img.svg" alt="download-img" className="resume__download-resume__img"/>
+                <img src="download-img.svg" alt="download-img" className="resume__download-resume__img" />
                 Download Resume
             </a>
+            <div className="resume__embedded-resume">
+                <embed src="Corey_Pierce-Resume.pdf#toolbar=0&zoom=100" className="resume__embedded-resume__view" />
+            </div>
         </div>
     );
 }
