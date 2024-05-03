496b4aeacc49810549773e2f2431bd85165f0b73
Cornfrog <iamcornfrog@gmail.com>
2024-05-02 20:14:40 -0400

    changed to iframe

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,9 +7,9 @@ export default function Resume() {
                 Download Resume
             </a>
             <div className="resume__embedded-resume">
-                <object data="Corey_Pierce-Resume.pdf" type="application/pdf" className="resume__embedded-resume__view">
-                    <p>PDF cannot be displayed. <a href="Corey_Pierce-Resume.pdf">Download</a> it instead.</p>
-                </object>
+                <iframe src="Corey_Pierce-Resume.pdf" className="resume__embedded-resume__view">
+                    This browser does not support PDFs. Please download the PDF to view it: <a href="Corey_Pierce-Resume.pdf">Download PDF</a>
+                </iframe>
             </div>
         </div>
     );
