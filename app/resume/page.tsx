449689c06a5de0587f508f5b3d3df6ca1ba5aaa0
Cornfrog <iamcornfrog@gmail.com>
2024-05-02 20:00:07 -0400

    changed embed to object to see if that resolves chrome mobile viewer

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,7 +7,9 @@ export default function Resume() {
                 Download Resume
             </a>
             <div className="resume__embedded-resume">
-                <embed src="Corey_Pierce-Resume.pdf#toolbar=0&zoom=100&view=FitH" className="resume__embedded-resume__view" />
+                <object data="Corey_Pierce-Resume.pdf" type="application/pdf" className="resume__embedded-resume__view">
+                    <p>PDF cannot be displayed. <a href="Corey_Pierce-Resume.pdf">Download</a> it instead.</p>
+                </object>
             </div>
         </div>
     );
